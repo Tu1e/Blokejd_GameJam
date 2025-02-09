c61887dc3e4c99c5429dd244c4f4eb680b8b2a21
Tu1e <77059041+Tu1e@users.noreply.github.com>
2025-02-09 13:43:24 +0100

    SVe

---
+++ Assets/Scripts/TransitionManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionManager : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+
+    private void OnEnable() {
+        PlayerManager.OnLvlFinished += StartTranition;
+    }
+
+    private void OnDisable(){
+        PlayerManager.OnLvlFinished -= StartTranition;
+    }
+
+    void StartTranition(){
+        animator.SetTrigger("FinishLevel");
+    }
+}
